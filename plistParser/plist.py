fcf795ba989f174d727447a1b06c42807c531522
Scy Productions <17727163+RealScyP@users.noreply.github.com>
2019-09-10 23:01:08 +0200

    Big changes in Parser.
    Parser is now capable of writing partially-correctly structured Python dictionaries.

--- plistParser/plist.py
+++ plistParser/plist.py
@@ -28,6 +28,10 @@ class PList:
 
             currentRoot = root
 
+            key = ""
+            value = ""
+            nextIsValue = False
+
             for line in dataLines:
                 if (line.startswith("<?") or line.startswith("<!") or line is ""):
                     continue
@@ -43,18 +47,55 @@ class PList:
                         continue
                     tagContent = content.split(">")[0]
 
+                    self.__debug(root)
+
                     if tagContent[0] is "/":
                         self.__debug(f"End tag <{tagContent}>")
+                        if tagContent == '/dict':
+                            self.__debug(f"/!\ This tag was a supported data holder. Restoring the root...")
+                            temp = previousRoot
+                            previousRoot = currentRoot
+                            currentRoot = temp
 
                     elif tagContent[len(tagContent)-1] is "/":
                         self.__debug(f"One use tag <{tagContent}>")
 
+                        if nextIsValue:
+                            self.__debug(f"/!\ This tag is a value!")
+                            if tagContent == 'true/':
+                                value = True
+                            elif tagContent == 'false/':
+                                value = False
+                            nextIsValue = False
+
                     else:
                         self.__debug(f"Start tag <{tagContent}>")
                         tagData = content.split(">")[1]
-                        if tagData is not '':
+                        if tagData != '':
                             self.__debug(f"Tag's data (inline): {tagData}")
-                    i+=1
+                        elif tagContent == 'dict':
+                            tagData = {}
+
+                        if nextIsValue:
+                            self.__debug(f"/!\ This tag is a value!")
+                            value = tagData
+                            currentRoot.update({key: value})
+                            self.__debug(f"Added new Key-Value: {str(key)}: {str(value)}")
+                            if tagContent == 'dict':
+                                nextIsValue = True
+                            nextIsValue = False
+
+                        if tagContent == 'dict' and nextIsValue:
+                            self.__debug(f"/!\ This tag is a supported data holder. Switching the root...")
+                            previousRoot = currentRoot
+                            currentRoot = tagData
+                            nextIsValue = False
+
+                        if tagContent == 'key':
+                            key = tagData
+                            self.__debug(f"Next tag is a value.")
+                            nextIsValue = True
+            return root
             # TODO: Parsing Logic [End]
 
             print(f"[PListParser] Parsed {self.file}!")
